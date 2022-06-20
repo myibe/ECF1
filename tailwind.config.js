d5016325ca190de3c4fd989a7709af0b592ace4a
myibe <myriamibed@gmail.com>
2022-06-20 16:33:18 +0200

    ECF 1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Italiana', 'serif'],
+    },
+    extend: {},
+  },
+  
+  plugins: [],
+}
